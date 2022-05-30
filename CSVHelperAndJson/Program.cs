44e8c902e6a1287a65d0c2618af7d562dc79c525
Rupali Natewad <rupalirnatewad@gmail.com>
2022-05-30 19:40:04 +0530

    Add project files.

---
+++ CSVHelperAndJson/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+using CSVHelperAndJson;
+
+Console.WriteLine("Hello, World!");
+Console.WriteLine("# Read data from CSV & Write data in CSV");
+CSVHandler.ImplementCSVDataHandling();
+ReadCSV_And_WriteJSON.ImplementCSVToJSON();
+Console.ReadKey();
